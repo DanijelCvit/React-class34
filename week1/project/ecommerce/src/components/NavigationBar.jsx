3608e1218275444c13d245d57e1e4f7a23b0a90b
Danijel C <danijelc+hyf@gmail.com>
2022-02-04 20:05:13 +0100

    Moved nav button to its own component

--- week1/project/ecommerce/src/components/NavigationBar.jsx
+++ week1/project/ecommerce/src/components/NavigationBar.jsx
@@ -1,22 +1,20 @@
 import React from "react";
 import categories from "../fake-data/all-categories.js";
+import Button from "./Button.jsx";
 
 const NavigationBar = ({ category, setCategory }) => {
   return (
     <nav>
       <ul className="flex items-stretch space-x-1">
         {categories.map((categoryItem) => (
-          <li className="bg-red-100" key={categoryItem}>
-            <button
-              onClick={() => setCategory(categoryItem)}
-              className={`h-full text-left px-4 py-2  font-bold ${
-                category === categoryItem ? "bg-gray-400" : "bg-gray-200"
-              } hover:bg-gray-300`}
-            >
-              {categoryItem}
-            </button>
+          <li key={categoryItem}>
+            <Button
+              category={category}
+              setCategory={setCategory}
+              categoryItem={categoryItem}
+            />
           </li>
-        ))}
+        ))}{" "}
       </ul>
     </nav>
   );
