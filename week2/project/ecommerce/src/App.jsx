03733ebb6636c979b65ec0ee2dffacf926c61525
Danijel C <danijelc+hyf@gmail.com>
2022-02-05 01:10:54 +0100

    Finished ecommerce II

---
+++ week2/project/ecommerce/src/App.jsx
@@ -0,0 +1,17 @@
+import NavigationBar from "./components/NavigationBar";
+import { useState } from "react";
+import ProductsList from "./components/ProductsList";
+
+function App() {
+  const [category, setCategory] = useState(null);
+
+  return (
+    <div className="App px-2">
+      <h1 className="font-bold text-3xl mt-5 mb-5">Products</h1>
+      <NavigationBar category={category} setCategory={setCategory} />
+      <ProductsList category={category} />
+    </div>
+  );
+}
+
+export default App;
---
+++ week2/project/ecommerce/src/hooks/useFetch.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        const res = await fetch(url);
+        const data = await res.json();
+        setData(data);
+      } catch (error) {
+        setErrorMessage(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { isLoading, data, errorMessage };
+};
+
+export default useFetch;
