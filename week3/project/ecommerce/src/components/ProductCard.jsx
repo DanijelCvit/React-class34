d2a926c0e6944bc6676f8f399aaf4d3f9241d64d
Danijel C <danijelc+hyf@gmail.com>
2022-02-06 21:28:00 +0100

    Fixed image stretching and added heart icons

--- week3/project/ecommerce/src/components/ProductCard.jsx
+++ week3/project/ecommerce/src/components/ProductCard.jsx
@@ -2,8 +2,15 @@ import React from "react";
 
 const ProductCard = ({ product }) => {
   return (
-    <div className="text-center mt-6">
-      <img src={product.image} alt="" className="mx-auto mb-2 h-72" />
+    <div className="text-center mt-6 relative">
+      <img
+        src={product.image}
+        alt=""
+        className="mx-auto mb-2 h-72"
+        style={{ objectFit: "contain" }}
+      />
+      <i className="far fa-heart fa-lg absolute top-2 right-2"></i>
+      <i className="fas fa-heart fa-lg absolute top-2 right-2"></i>
       <h2 className="text-ellipsis overflow-hidden whitespace-nowrap">
         {product.title}
       </h2>
